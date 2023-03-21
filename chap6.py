4cf0264e9de07d2eed3a4b973cf40ac21c67846f
gabiemma <gabrielledja2014@gmail.com>
2023-03-20 22:08:49 -0500

    first commit

---
+++ chap6.py
@@ -0,0 +1,9 @@
+x = 5
+print("A spoon", end=" ")
+if x == 4:
+    print("full of", end=" ")
+elif x>=4:
+    print("sugar", end=" ")
+else:
+    print("helps the medecine", end=" ")
+print("go down", end=" ")
